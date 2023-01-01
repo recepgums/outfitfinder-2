62c07a9d9224adcec02e2ebb56e850932097c052
Recep Gumus <recepgumusyasin@gmail.com>
2023-01-01 19:09:51 +0300

    layout folder

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <div class=" d-none d-md-block">
+      <nav class="navbar navbar-light justify-content-between">
+        <NuxtLink class="navbar-brand" to="/">
+          <h1 class="text-center">
+            closet
+            <span style="
+            color: white;
+            background-color: deeppink;
+            border-radius: 20px;
+            padding: 5px 10px">
+              finder
+            </span>
+          </h1>
+        </NuxtLink>
+
+        <form class="form-inline">
+          <input class="form-control mr-sm-2" type="search" placeholder="Celebrities, Closets, Jewelry..." aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </nav>
+    </div>
+
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  overflow-y: auto
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
