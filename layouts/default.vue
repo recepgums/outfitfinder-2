a09575886b1060f850570c4a6e5de9c9ca1c01c1
Recep Gumus <recepgumusyasin@gmail.com>
2023-01-05 16:26:47 +0300

    nuxt sitemap

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
       <NuxtLink class="navbar-brand" to="/">
         <h3 class="text-center">
           closet
@@ -23,7 +23,7 @@
           <li class="nav-item">
             <NuxtLink class="nav-link" to="/feed">Feed</NuxtLink>
           </li>
-          <li class="nav-item active">
+          <li class="nav-item">
             <NuxtLink class="nav-link" to="/celebrity">Celebrities</NuxtLink>
           </li>
           <li class="nav-item dropdown">
@@ -65,58 +65,51 @@ export  default {
 
 <style>
 html {
-a.nuxt-link-active {
-  font-weight: bold;
-}
-/* exact link will show the primary color for only the exact matching link */
-a.nuxt-link-exact-active {
-  color: #00c58e;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  overflow-y: auto
 }
 
-body {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-  Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
-  Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
   margin: 0;
 }
 
-main {
-  margin: 0 auto;
-  padding: 0 1rem;
-  margin-top: 100px;
-  max-width: 1280px;
-  text-align: center;
-}
-img {
-  margin-bottom: 1rem;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-li {
-  margin: 0 0.5rem;
-  padding: 0.25rem;
-  font-size: 1.2rem;
-}
-
-nav {
-  padding: 0 1rem;
-}
-
-a,
-a:visited {
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
   text-decoration: none;
-  color: inherit;
+  padding: 10px 30px;
 }
 
-a:hover {
-  color: #00c58e;
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
 }
 
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
 }
 </style>
