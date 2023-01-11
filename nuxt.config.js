e127ad2b5cde0913a85018312d28aa3f5d967165
Recep Gumus <recepgumusyasin@gmail.com>
2023-01-11 22:19:01 +0300

    url changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,7 @@ export default {
 
   image: {
     format: 'webp',
-    domains: ['dijitaluzmaniniz.com'],
+    domains: ['http://54.172.64.77'],
     provider: 'ipx'
   },
 
@@ -93,7 +93,7 @@ export default {
   },
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://dijitaluzmaniniz.com/api/'
+    baseURL: 'http://54.172.64.77/api/'
   },
 
   device: {
