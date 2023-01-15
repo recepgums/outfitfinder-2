527a566ef5c1bb9a4669f4dbb4aa8232363a00af
Recep Gumus <recepgumusyasin@gmail.com>
2023-01-15 21:19:35 +0300

    font-display: swap;

--- layouts/default.vue
+++ layouts/default.vue
@@ -64,13 +64,14 @@ html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
   Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
+  font-display: swap;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-  overflow-y: auto
+  overflow-y: auto;
 }
 
 *,
