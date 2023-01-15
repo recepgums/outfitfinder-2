ae27835a322952783ea429d4074846235a6bf982
Recep Gumus <recepgumusyasin@gmail.com>
2023-01-15 21:58:05 +0300

    domain changed api.clostefinder.com

--- nuxt.config.js
+++ nuxt.config.js
@@ -94,7 +94,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: 'http://127.0.0.1:8000/api/'
-    baseURL: 'https://dijitaluzmaniniz.com/api/'
+    baseURL: 'https://api.closetfinder.com/api/'
   },
 
   device: {
